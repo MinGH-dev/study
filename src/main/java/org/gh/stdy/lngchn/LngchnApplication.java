1fe13f7e15c4649defb0ce13653fc062385f71ef
dlsqns <dlsqns@gmail.com>
2024-02-18 18:28:04 +0900

    최초 업로드

---
+++ src/main/java/org/gh/stdy/lngchn/LngchnApplication.java
@@ -0,0 +1,13 @@
+package org.gh.stdy.lngchn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LngchnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LngchnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gh/stdy/lngchn/LngchnApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gh.stdy.lngchn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LngchnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
